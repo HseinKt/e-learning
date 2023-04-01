437b66fbe9842ef6cbacd1577ea64030a9e0c90d
HseinKt <hseinkteish1998@gmail.com>
2023-04-02 00:55:41 +0300

    fixed mongoDB

--- back_end/index.js
+++ back_end/index.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const app = express();
+require("dotenv").config(); // call the config() method to load the environment variables from the .env file:
+app.use(express.json());
+
+// const authRouter = require("./routes/auth.routes");
+// app.use('/auth',authRouter);
+
+app.listen(process.env.PORT, (err) => {
+    if (err) console.log(err);
+    console.log("Server is running on port ", process.env.PORT);
+    require("./configs/db.config")
+});
