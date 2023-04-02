9bb6dc4f6655b9c829df0477c4d3cda51d5fb12c
HseinKt <hseinkteish1998@gmail.com>
2023-04-02 23:45:07 +0300

    added middlewares

---
+++ back_end/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+exports.authMiddleware = async (req, res, next) => {
+
+    try {
+        const token = req.headers.authorization?.split(" ")[1]
+        
+        if( !token ) return res.status(403).json({
+            message: "Unauthenticated"
+        })
+
+        const decode = jwt.verify(token, process.env.SECRET_KEY)
+        const user = await User.findById(decode.id, "-password")
+
+        req.user = user
+        
+        next()
+    } catch (error) {
+        return res.status(500).json({
+            message: "Server Error"
+        })
+    }
+}
