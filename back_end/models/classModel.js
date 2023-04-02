59652fda8ca49edc93ad2dd3de19e49c2a50a63c
HseinKt <hseinkteish1998@gmail.com>
2023-04-02 05:54:05 +0300

    added class model

---
+++ back_end/models/classModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const classSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    start_date: {
+        type: Date,
+        default: Date.now,
+    },
+    end_date: {
+        type: Date,
+    }
+})
+
+const Class = mongoose.model("Class",classSchema);
+
+module.exports = {Class};
