defa3c3cea712f14b59bd7380e19f131595ed3a2
HseinKt <hseinkteish1998@gmail.com>
2023-04-04 01:13:53 +0300

    added all Routes

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,13 +2,19 @@ import { Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/landing_page";
 import WithdrawingPage from "./pages/withdrawing_page";
 import EnrollingPage from "./pages/enrolling_page";
+import Login from "./components/login_page/login";
+import Register from "./components/register_page/register";
+import NotFound from "./components/not_fount/not_found";
 
 function App() {
   return (
     <Routes>
       <Route path="/enroll" element={ <EnrollingPage /> }/>
       <Route path="/withdraw" element={ <WithdrawingPage /> }/>
+      <Route path="login" element={ <Login /> } />
+      <Route path="register" element={ <Register /> } />
       <Route path="/" element={ <LandingPage /> } />
+      <Route path="*" element={ <NotFound /> } />
     </Routes>
   );
 }
--- front_end/src/components/not_fount/not_found.js
+++ front_end/src/components/not_fount/not_found.js
@@ -1,4 +1,4 @@
-const Login = () => {
+const NotFound = () => {
     return ( 
         <div>
             
@@ -6,4 +6,4 @@ const Login = () => {
      );
 }
  
-export default Login;
+export default NotFound;
