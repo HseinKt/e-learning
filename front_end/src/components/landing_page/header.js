b5836d7803e4b09a463fc387fc04ad18edc6fb0b
HseinKt <hseinkteish1998@gmail.com>
2023-04-17 03:32:49 +0300

    added logout in the header component

--- front_end/src/components/landing_page/header.js
+++ front_end/src/components/landing_page/header.js
@@ -3,16 +3,38 @@ import Logo from "./logo";
 
 const Header = () => {
     const navigate = useNavigate();
-    return ( 
-        <header className="header">
-            <Logo />
-            <h1>E-LEARNING</h1>
-            <div className="LogReg">
-                <button className="login btn" onClick={() => navigate("../login")}>Login</button>
-                <button className="register btn" onClick={() => navigate("../register")}>Register</button>
-            </div>
-        </header>
-     );
+
+    const token = localStorage.getItem('token');
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate("../login");
+    }
+    
+    if(token) {
+        return ( 
+            <header className="header">
+                <Logo />
+                <h1>E-LEARNING</h1>
+                <div className="LogReg">
+                    <button className="login btn" onClick={() => handleLogout()}>Logout</button>
+                </div>
+            </header>
+         );
+    }
+    else {
+        return ( 
+            <header className="header">
+                <Logo />
+                <h1>E-LEARNING</h1>
+                <div className="LogReg">
+                    <button className="login btn" onClick={() => navigate("../login")}>Login</button>
+                    <button className="register btn" onClick={() => navigate("../register")}>Register</button>
+                </div>
+            </header>
+         );
+    }
+    
 }
  
 export default Header;
